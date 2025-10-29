5f8e511ea5c3c10cf9d2cd02062e9740925959bf
TechNik004 <gorka8212@gmail.com>
2025-10-29 17:50:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -83,7 +83,7 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 # Verification Settings
 # ============================
 VERIFY = bool(environ.get('VERIFY', True))  # Verification On (True) / Off (False)
-VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 8))  # Add time in hours
+VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 10))  # Add time in hours
 VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1003027241472'))  # Log channel id (make sure bot is admin)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/bot_verify_video/12')  # How to open tutorial link for verification
 
@@ -92,7 +92,7 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/bot_verify_video/12')
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkshortify.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '53229e400eba26696058cd2640bd120261864a01')
+SHORTLINK_API = environ.get('SHORTLINK_API', '181feb5a18a1e702e5ccf4caf1cac37ebb607822')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/bot_verify_video/12')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
